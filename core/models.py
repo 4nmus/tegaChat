ce279912e2e65d2331db029dfe835eb14649d0bc
4nmus <andr_cyp@proton.me>
2024-12-19 21:40:28 +0100

    Basic fucntions

--- core/models.py
+++ core/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
+
 # Create your models here.
 
 class Message(models.Model):
     text = models.TextField("Text", max_length=300)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     date = models.DateTimeField(default=timezone.now)
 
 
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Message
 
 # Create your views here.
+
 
 def main(request):
     return render(request, 'core/main.html')
