c0a5ed98e63ecb2d652e2a76e16ffed33af44f0a
4nmus <andr_cyp@proton.me>
2025-01-11 10:13:55 +0100

    quick check

--- tegaChat/settings.py
+++ tegaChat/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!p
-#SECRET_KEY = 'django-insecure-w!jyoe0of2z7=a%=nz=3$*(z82nuys669wht%wvcjfi30mr&$5'
+SECRET_KEY = 'django-insecure-w!jyoe0of2z7=a%=nz=3$*(z82nuys669wht%wvcjfi30mr&$5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
