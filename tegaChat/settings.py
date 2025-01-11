30264235551a7df578ce01e230c5c0ead38bd0bb
4nmus <andr_cyp@proton.me>
2025-01-11 10:24:02 +0100

    static files and migrations

--- tegaChat/settings.py
+++ tegaChat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -114,6 +115,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
@@ -122,3 +126,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#used for static folder with all css
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, '..', 'static')
+]
+
+MEDIA_URL = "/pictures/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures')
